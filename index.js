4d296051f4e653d01860f4c151b51d7568a82e7b
Jim Ireland <jireland@outlook.com>
2023-07-21 08:23:48 +0100

    Fully working example, only runs on local machine

---
+++ index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+
+(async()=> {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const html = fs.readFileSync('sample.html', 'utf-8');
+
+
+    await page.setContent(html, { waitUntil:'domcontentloaded'});
+
+    await page.emulateMediaType('screen');
+
+    const pdf = await page.pdf({
+        path: 'result.pdf',
+        margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+        printBackground: true,
+        format: 'A4',
+    });
+
+    await browser.close()
+})();
